8a17aa3abced51f39a165cf52fa4d965051e7d33
Derick Rethans <github@derickrethans.nl>
2023-06-22 19:00:40 +0100

    Separated interfaces into SeqCollection and DictCollection

--- Zend/zend_object_handlers.c
+++ Zend/zend_object_handlers.c
@@ -1020,7 +1020,9 @@ ZEND_API zval *zend_std_read_dimension(zend_object *object, zval *offset, int ty
 			return NULL;
 		}
 		return rv;
-	} else if (zend_class_implements_interface(ce, zend_ce_collection)) {
+	} else if (zend_class_implements_interface(ce, zend_ce_seq_collection)) {
+		return zend_collection_read_item(object, offset);
+	} else if (zend_class_implements_interface(ce, zend_ce_dict_collection)) {
 		return zend_collection_read_item(object, offset);
 	} else {
 	    zend_bad_array_access(ce);
@@ -1045,7 +1047,9 @@ ZEND_API void zend_std_write_dimension(zend_object *object, zval *offset, zval *
 		zend_call_known_instance_method_with_2_params(funcs->zf_offsetset, object, NULL, &tmp_offset, value);
 		OBJ_RELEASE(object);
 		zval_ptr_dtor(&tmp_offset);
-	} else if (zend_class_implements_interface(ce, zend_ce_collection)) {
+	} else if (zend_class_implements_interface(ce, zend_ce_seq_collection)) {
+		zend_collection_add_item(object, offset, value);
+	} else if (zend_class_implements_interface(ce, zend_ce_dict_collection)) {
 		zend_collection_add_item(object, offset, value);
 	} else {
 	    zend_bad_array_access(ce);
@@ -1073,7 +1077,9 @@ ZEND_API int zend_std_has_dimension(zend_object *object, zval *offset, int check
 		}
 		OBJ_RELEASE(object);
 		zval_ptr_dtor(&tmp_offset);
-	} else if (zend_class_implements_interface(ce, zend_ce_collection)) {
+	} else if (zend_class_implements_interface(ce, zend_ce_seq_collection)) {
+		return zend_collection_has_item(object, offset);
+	} else if (zend_class_implements_interface(ce, zend_ce_dict_collection)) {
 		return zend_collection_has_item(object, offset);
 	} else {
 	    zend_bad_array_access(ce);
@@ -1258,7 +1264,9 @@ ZEND_API void zend_std_unset_dimension(zend_object *object, zval *offset) /* {{{
 		zend_call_known_instance_method_with_1_params(funcs->zf_offsetunset, object, NULL, &tmp_offset);
 		OBJ_RELEASE(object);
 		zval_ptr_dtor(&tmp_offset);
-	} else if (zend_class_implements_interface(ce, zend_ce_collection)) {
+	} else if (zend_class_implements_interface(ce, zend_ce_seq_collection)) {
+		zend_collection_unset_item(object, offset);
+	} else if (zend_class_implements_interface(ce, zend_ce_dict_collection)) {
 		zend_collection_unset_item(object, offset);
 	} else {
 	    zend_bad_array_access(ce);
